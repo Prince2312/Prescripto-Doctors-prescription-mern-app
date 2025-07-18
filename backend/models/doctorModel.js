2b64bafb45f4bfcbaa3c40b84eac308f21c901ce
Prince2312 <prince8756patel@gmail.com>
2025-07-18 13:21:28 +0530

    next commit

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import mangoose from 'mongoose';
+
+const doctorSchema = new mongoose.Schema({
+    name:{type:String,require:true},
+    email:{type:String,require:true,unique:true},
+    passwors:{type:String,require:true},
+    image:{type:String,require:true},
+    speciality:{type:String,require:true},
+    degree:{type:String,require:true},
+    experience:{type:String,require:true},
+    about:{type:String,require:true},
+    available:{type:Boolean,require:true},
+    fees:{type:Number,require:true},
+    address:{type:Object,require:true},
+    date:{type:Number,require:true},
+    slots_booked:{type:Object,default:{}},
+},{minimize:false})
